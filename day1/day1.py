d52a12787cf0634a9a27b24434ae02ac5fec6147
MurrayCode <Murraystewart94@hotmail.co.uk>
2020-12-14 10:59:08 +0000

    day 1 task 1 finished

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,23 @@
+import csv
+import functools
+import operator
 
 numbers = ['1721', '979', '366', '299', '675', '1456']
+with open('input.csv')as f:
+    reader = csv.reader(f, delimiter='\n')
+    input = list(reader)
+    merged = functools.reduce(operator.iconcat, input, [])
 
 def sum(nums):
     for i in range(len(nums)):
         for j in range(len(nums)):
-            if j+1 <= len(nums):
+            if j+1 != len(nums):
                 if int(nums[i]) + int(nums[j+1]) == 2020:
                     print(nums[i] + " + " + nums[j+1] + " = 2020")
                     return print(int(nums[i]) * int(nums[j+1]))
                 else:
                     print("does not equal 2020")
+                    print(j)
 
-
-
-sum(numbers)
+print(merged)
+sum(merged)
