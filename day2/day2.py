c8c6639412ae0c3a68c0053ba3497079f7bb6d33
MurrayCode <Murraystewart94@hotmail.co.uk>
2020-12-14 12:31:21 +0000

    day 2 - second problem solved

--- day2/day2.py
+++ day2/day2.py
@@ -25,4 +25,19 @@ def check(list):
         
     return print(validcount)
 
-check(merged)
+def checktwo(list):
+    validcount = 0
+    for pwcheck in merged:
+        split = pwcheck.split()
+        nums = split[0].split("-")
+        first = int(nums[0]) -1 
+        second = int(nums[1]) -1
+        letter = str(split[1].replace(':', ''))
+        pw = split[2]
+        if pw[first] == letter and pw[second] != letter:
+            validcount += 1
+        if pw[second] == letter and pw[first] != letter:
+            validcount += 1
+    return print(validcount)
+#check(merged)
+checktwo(merged)
