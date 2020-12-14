6bf65247e3f625b20283aa279a4d0fbbbe1d94b5
MurrayCode <Murraystewart94@hotmail.co.uk>
2020-12-14 11:19:58 +0000

    day 1 - second problem solved

--- day1/day1.py
+++ day1/day1.py
@@ -11,13 +11,25 @@ with open('input.csv')as f:
 def sum(nums):
     for i in range(len(nums)):
         for j in range(len(nums)):
-            if j+1 != len(nums):
-                if int(nums[i]) + int(nums[j+1]) == 2020:
-                    print(nums[i] + " + " + nums[j+1] + " = 2020")
-                    return print(int(nums[i]) * int(nums[j+1]))
+                if int(nums[i]) + int(nums[j]) == 2020:
+                    print(nums[i] + " + " + nums[j] + " = 2020")
+                    return print(int(nums[i]) * int(nums[j]))
                 else:
                     print("does not equal 2020")
                     print(j)
 
-print(merged)
+
+def trisum(nums):
+    length = len(nums)
+    for i in range(length):
+        for j in range(length):
+            for x in range(length):
+                    if int(nums[i]) + int(nums[j]) + int(nums[x]) == 2020:
+                        print(nums[i] + " + " + nums[j] + " + " + nums[x] + " = 2020")
+                        return print(int(nums[i]) * int(nums[j]) * int(nums[x]))
+                    else:
+                        print("does not equal 2020")
+                        print(j)
+                        print(x)
 sum(merged)
+trisum(merged)
